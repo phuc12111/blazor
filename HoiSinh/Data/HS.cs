e7d6afd883b9745927efbfb81aa43c19aa70fc31
Dinh Phuc <ngodinhphuc1211@gmail,com>
2024-03-07 11:37:23 +0700

    hoi sinh

---
+++ HoiSinh/Data/HS.cs
@@ -0,0 +1,17 @@
+﻿namespace HoiSinh.Data
+{
+    public class HS
+    {
+        public string Code { get; set; }
+        public string display { get; set; }
+    }
+    public class ApiResult<T>
+    {
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string Hints { get; set; }
+        public T Value { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ HoiSinh/Program.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<HttpClient>(s =>
+{
+    var navigationManager = s.GetRequiredService<NavigationManager>();
+    return new HttpClient { BaseAddress = new Uri(navigationManager.BaseUri) };
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
